92eff4cee259535e7ae00c58ad5cb873b83ceaf0
AjiNurAji <ajinuraji090306@gmail.com>
2025-01-10 16:21:09 +0700

    add songs

--- src/components/song.tsx
+++ src/components/song.tsx
@@ -1,23 +1,17 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import song1 from "../assets/song/Nothing-Gonna-Cahnge-Saxofone.mp3";
 
 const Song: React.FC = () => {
-    const audioRef = useRef<HTMLAudioElement | null>(null);
-
-    useEffect(() => {
-        if (!audioRef.current) return;
-
-        audioRef.current.play();
-    }, [])
-
-    return (
-        <div className="sr-only">
-            <audio autoPlay loop ref={audioRef}>
-                <source src={song1} type="audio/mp3" />
-                Your browser does not support the audio element.
-            </audio>
-        </div>
-    )
-}
+	return (
+		<div
+			className="sr-only"
+		>
+			<audio autoPlay loop id="song">
+				<source src={song1} type="audio/mpeg" />
+				Your browser does not support the audio element.
+			</audio>
+		</div>
+	);
+};
 
 export default Song;
